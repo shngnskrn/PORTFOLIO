7e0678fd8a41ab57a5ddb5afb49513790caccae2
shngnskrn <dilgun01@easv365.dk>
2026-04-22 16:53:04 +0200

    cv update

--- dg-portfolio/vite.config.js
+++ dg-portfolio/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ViteImageOptimizer from 'vite-plugin-image-optimizer'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue(), ViteImageOptimizer()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
